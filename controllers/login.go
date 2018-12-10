4b5a8e9f20b2f9ca4e69c81357abeb9fb6e4c0f0
leslie <821753585@qq.com>
2018-12-10 14:07:51 +0800

    delete filter

--- controllers/login.go
+++ controllers/login.go
@@ -66,22 +66,6 @@ func (c *RegisterController) Register(){
 }
 
 
-func (c *LoginController) FilterUser(){
-	_, ok := c.Ctx.Input.Session("uid").(int)
-	if !ok && c.Ctx.Request.RequestURI != "/login" {
-		c.Ctx.Redirect(302, "/login")
-	}
-}
-
-var FilterUser = func(ctx *context.Context) {
-	_, ok := ctx.Input.Session("uid").(int)
-	if !ok && ctx.Request.RequestURI != "/login" {
-		ctx.Redirect(302, "/login")
-	}
-}
-
-
-
 
 
 
